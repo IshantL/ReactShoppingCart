188649bcd04bc7670b6ca7f9d5871c90e2ced23f
IshantL <ishantlambhate@gmail.com>
2020-02-19 12:48:45 +0530

    adding components

--- ecommerce-shopping-cart/src/App.js
+++ ecommerce-shopping-cart/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Products from './components/Products';
+import Filter from './components/Filter';
 import './App.css';
 
 class App extends Component{
@@ -26,13 +27,54 @@ class App extends Component{
     });
   }
 
+  listProducts = () => {
+    this.setState(state => {
+      if (state.sort !== "") {
+        state.products.sort((a, b) =>
+          state.sort === "lowestprice"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : a.price < b.price
+            ? 1
+            : -1
+        );
+      } else {
+        state.products.sort((a, b) => (a.id > b.id ? 1 : -1));
+      }
+      if (state.size !== "") {
+        return {
+          filteredProducts: state.products.filter(
+            a => a.availableSizes.indexOf(state.size.toUpperCase()) >= 0
+          )
+        };
+      }
+      return { filteredProducts: state.products };
+    });
+  };
+
+  handleSortChange = e => {
+    this.setState({ sort: e.target.value });
+    this.listProducts();
+  };
+  handleSizeChange = e => {
+    this.setState({ size: e.target.value });
+    this.listProducts();
+  };
+
   render(){
     return (
       <div className="container">
         <h1>Shopping Cart Application</h1>
         <hr/>
         <div className="row">
-          <div>
+        <div className="col-md-9">
+            <Filter
+              count={this.state.products.length}
+              handleSortChange={this.handleSortChange}
+              handleSizeChange={this.handleSizeChange}
+            />
+            <hr />
           <Products
               products={this.state.products}
               handleAddToCart={this.handleAddToCart}
