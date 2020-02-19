94c3366305335405927962a3ce87451f3f06440a
IshantL <ishantlambhate@gmail.com>
2020-02-19 11:25:59 +0530

    fetch logic

--- ecommerce-shopping-cart/src/App.js
+++ ecommerce-shopping-cart/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Products from './components/Products';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      size: "",
+      sort: "",
+      cartItems: [],
+      products: [],
+      filteredProducts: []
+    };
+  }
+  handleAddToCart = () =>{
+    console.log("handle cart");
+  }
+
+  componentWillMount(){
+    fetch("http://localhost:8085/products")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      this.setState({ products: data });
+    });
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <h1>Shopping Cart Application</h1>
+        <hr/>
+        <div className="row">
+          <div>
+          <Products
+              products={this.state.products}
+              handleAddToCart={this.handleAddToCart}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
